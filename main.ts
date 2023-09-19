feaf116f8b4c75b22bd8762a2cc085532dc62257
Yuki <github@risaton.net>
2023-09-20 00:41:42 +0900

    commit

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
-import 'discord.js'
-import { Client, Events, GatewayIntentBits } from 'discord.js'
-
+// import 'discord.js'
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js'
+declare module "discord.js" {
+    export interface Client {
+      commands: Collection<any, any>;
+    }
+  }
 require('dotenv').config()
 
 console.log('hello')
@@ -12,6 +16,10 @@ const client = new Client({
         GatewayIntentBits.GuildVoiceStates
     ]
 })
+
+client.commands = new Collection();
+
+
 
 client.once(Events.ClientReady, c =>{
     console.log(`Logged in as: ${c.user.tag}`)
