8d6b085e4f6e96068f088cde151176ba6cdedb5c
Yuki <github@risaton.net>
2023-09-20 00:31:36 +0900

    commit

--- main.ts
+++ main.ts
@@ -1,5 +1,20 @@
 import 'discord.js'
+import { Client, Events, GatewayIntentBits } from 'discord.js'
 
 require('dotenv').config()
 
 console.log('hello')
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates
+    ]
+})
+
+client.once(Events.ClientReady, c =>{
+    console.log(`Logged in as: ${c.user.tag}`)
+} )
+
+client.login(process.env.DISCORD_TOKEN)
