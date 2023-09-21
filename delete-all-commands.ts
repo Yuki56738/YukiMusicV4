307fd48722872965a45b48191aff10f8cdcc612a
Yuki <github@risaton.net>
2023-09-21 23:45:31 +0900

    commit

--- delete-all-commands.ts
+++ delete-all-commands.ts
@@ -1,48 +1,21 @@
-import { REST } from 'discord.js';
-import fs from 'node:fs'
-import path from 'node:path'
+require('dotenv').config();
 
-const { Routes } = require('discord.js')
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 
-require('dotenv').config()
-
-let token:string = "";
-let clientId: string = "";
-
-if (process.env.DISCORD_TOKEN != undefined){
-    token = process.env.DISCORD_TOKEN;
-}
-if (process.env.DISCORD_CLIENT_ID != undefined){
-    clientId = process.env.DISCORD_CLIENT_ID;
-}
-
-const commands: any[] = [];
-
-const foldersPath = path.join(__dirname, 'commands')
-const commandFolders = fs.readdirSync(foldersPath)
-
-// for (const folder of commandFolders){
-//     const commandsPath = path.join(foldersPath)
-//     const commandFiles = fs.readdirSync(commandsPath)
-//     for (const file of commandFiles){
-//         const filePath = path.join(commandsPath, file)
-//         const command = require(filePath)
-//         commands.push(command.data.toJSON())
-//     }
-// }
-const rest: REST = new REST().setToken(token);
-
-(async () =>{
-    try{
-      rest.put(Routes.applicationGuildCommands(clientId, '977138017095520256'), { body: [] })
-      .then(() => console.log('Successfully deleted all guild commands.'))
-      .catch(console.error);
+const token = process.env.DISCORD_TOKEN;
+const clientId = process.env.DISCORD_CLIENT_ID;
+const guildId = process.env.TEST_GUILD_ID;
     
-    // for global commands
-    rest.put(Routes.applicationCommands(clientId), { body: [] })
-      .then(() => console.log('Successfully deleted all application commands.'))
-      .catch(console.error);
-    }catch(error){
-        console.error(error);
-    }
-})();
+const rest = new REST({ version: '9' }).setToken(token);
+rest.get(Routes.applicationCommands(clientId))
+    .then((data: any) => {
+        const promises = [];
+        for (const command of data) {
+            const deleteUrl = `${Routes.applicationGuildCommands(clientId)}/${command.id}`;
+
+            promises.push(rest.delete(deleteUrl));
+        }
+        return Promise.all(promises);
+    });
